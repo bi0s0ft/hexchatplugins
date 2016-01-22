ce68fcc9c2d33da466787e1daf9455c0268f97d9
Cheraphy <Cheraphy@gmail.com>
2016-01-22 10:59:15 -0600

    changed line 13 for consistency

--- greentext.py
+++ greentext.py
@@ -1,7 +1,7 @@
 import hexchat
 
 __module_name__ = ">greentext"
-__module_version__ = "1.0.0.0"
+__module_version__ = "1.0.1.3"
 __module_description__ = "adds 4chan style greentext to hexchat"
 
 
@@ -10,7 +10,7 @@ def greentext(word, word_eol, userdata):
 	text = word_eol[0]
 
 	if text.find(">") > -1:
-		if word_eol[0].index(">") == 0:
+		if text.index(">") == 0:
 			text = word_eol[0].replace(">", "\00303>")
 	
 	hexchat.command(" ".join(["msg", hexchat.get_info("channel"), text]))
