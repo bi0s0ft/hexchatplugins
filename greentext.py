63e8203b949bfbfe5013e3920a66d1921894d507
Cheraphy <Cheraphy@gmail.com>
2016-01-22 12:14:01 -0600

    remembered python comparitive and/or short circuit, changed code for brevity

--- greentext.py
+++ greentext.py
@@ -1,7 +1,7 @@
 import hexchat
 
 __module_name__ = ">greentext"
-__module_version__ = "1.0.1.4"
+__module_version__ = "1.0.1.5"
 __module_description__ = "adds 4chan style greentext to hexchat"
 
 
@@ -9,8 +9,7 @@ __module_description__ = "adds 4chan style greentext to hexchat"
 def greentext(word, word_eol, userdata):
 	text = word_eol[0]
 
-	if text.find(">") > -1:
-		if text.index(">") == 0:
+	if text.find(">") > -1 and text.index(">") == 0:
 			text = word_eol[0].replace(">", "\00303>")
 	
 	hexchat.command(" ".join(["msg", hexchat.get_info("channel"), text]))
